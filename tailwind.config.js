72669b6ce4aba0dad502bce64f4f2a04e6d2c153
Grey Milton's MBP <grey.milton@gmail.com>
2023-05-13 15:51:14 +0930

    style the start of main content basically

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,8 @@ module.exports = {
         'rundle-header-pink': '#ebcbc3',
         'rundle-grey-50': '#ebe9e6',
         'rundle-grey-800': '#4b4946',
+        'rundle-temporary-green': '#afb854',
+        'rundle-blue-300': '#bfcce3',
       }
     },
   },
