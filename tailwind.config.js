cde76a294be6e7ad41714cdb77393d463912303f
Grey Milton's MBP <grey.milton@gmail.com>
2023-05-13 15:04:55 +0930

    style header basically

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'rundle-header-pink': '#ebcbc3',
+        'rundle-grey-50': '#ebe9e6',
+        'rundle-grey-800': '#4b4946',
+      }
+    },
   },
   plugins: [],
 }
