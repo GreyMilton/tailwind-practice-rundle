3c1209450d39c19f222495c99bc3b427e2b24d43
Grey Milton's MBP <grey.milton@gmail.com>
2023-05-14 07:44:09 +0930

    style second grid simply

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,11 @@ module.exports = {
     extend: {
       colors: {
         'rundle-header-pink': '#ebcbc3',
+        'rundle-body-pink': '#ffc4c9',
+        'rundle-body-tan': '#e9dec7',
         'rundle-grey-50': '#ebe9e6',
         'rundle-grey-800': '#4b4946',
+        'rundle-grey-900': '#2d3639',
         'rundle-temporary-green': '#afb854',
         'rundle-blue-300': '#bfcce3',
       }
