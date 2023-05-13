f0b28d4f572949f8651917f80a310aeeeada67d9
Grey Milton's MBP <grey.milton@gmail.com>
2023-05-14 08:50:19 +0930

    start styling the history and offers grid

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,8 @@ module.exports = {
         'rundle-header-pink': '#ebcbc3',
         'rundle-body-pink': '#ffc4c9',
         'rundle-body-tan': '#e9dec7',
+        'rundle-body-green': '#828c26',
+        'rundle-body-brown': '#b69c7d',
         'rundle-grey-50': '#ebe9e6',
         'rundle-grey-800': '#4b4946',
         'rundle-grey-900': '#2d3639',
